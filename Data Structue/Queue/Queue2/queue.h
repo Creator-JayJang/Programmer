ded16bee702d7cc7004c592aa0e3f2c48c27bb82
Jay <jjj90750@naver.com>
2020-06-30 19:08:15 +0900

    Uploading

--- Data Structue/Queue/Queue2/queue.h
+++
@@ -1,7 +0,0 @@
-#ifndef QUEUE_H		// Duplication 
-#define QUEUE_H
-
-void push(int);
-int pop(void);
-
-#endif
--- Data Structue/Stack/Stack2/stack.h
+++
@@ -1,7 +0,0 @@
-#ifndef STACK_H		// Duplication 
-#define STACK_H
-
-void push(int);
-int pop(void);
-
-#endif
