5a0daf78a4b3b2c36f14ffeabe697a822c65afb0
Creator-JayJang <67460101+Creator-JayJang@users.noreply.github.com>
2020-07-28 17:29:09 +0900

    Object Oriented Programming

---
+++ Python Programming Language/notebook.py
@@ -0,0 +1,60 @@
+class Note(object):
+    def __init__(self, contents):
+        self.contents = contents
+        
+    def write_contents(self, contents):
+        self.contents = contents
+        
+    def get_number_of_lines(self):
+        return self.contents.count("\n")
+    
+    def get_number_of_characters(self):
+        return len(self.contents)
+    
+    def remove(self):
+        self.contents = "This is Deleted Note"
+        
+    def __str__(self):
+        return self.contents
+    
+class NoteBook(object):
+    def __init__(self,name):
+        self.name = name
+        self.pages = 0
+        self.notes = {}
+        
+    def add_note(self, note, page_number=0):
+        if len(self.notes.keys()) < 300:
+            if page_number == 0:
+                # if self.pages < 301:
+                self.notes[self.pages] = note
+                self.pages += 1
+                
+            else:
+                if page_number not in self.notes.keys():
+                    self.notes[page_number] = note
+                else:
+                    print(" Note exist in this page already")
+        else:
+            print("You cannot add more Note")
+            
+    def remove_note(self, page_number):
+        del self.notes[page_number]
+        
+    def get_number_of_all_lines(self):
+        result = 0
+        for k in self.notes.keys():
+            result += self.notes[k].get_number_of_lines()
+        return result
+    
+    def get_number_of_all_characters(self):
+        result = 0
+        for k in self.notes.keys():
+            result += self.notes[k].get_number_of_characters()
+        return result
+    
+    def get_number_of_all_pages(self):
+        return len(self.notes.keys())
+    
+    def __str__(self):
+        return self.name
